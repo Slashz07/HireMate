6ec0cab9f4d04564c6f5062888de87b2deee3493
Sumit Saini <sumitforsaken76248@gmail.com>
2026-01-13 10:05:53 +0530

    project setup | shadcn components added

---
+++ src/components/myComponents/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='bg-muted/50 py-12 text-center'>
+        © 2026 careerCoach. All rights reserved.
+    </div>
+  )
+}
+
+export default Footer
--- src/my-app/app/layout.tsx
+++ src/my-app/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/ui/theme-provider";
-import Footer from "@/components/myComponents/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,19 +23,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <main className="min-h-screen">{children}</main>
-          <Footer/>
-        </ThemeProvider>
+        {children}
       </body>
     </html>
   );
