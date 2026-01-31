e68eb2b8d48a3ed5fb7c004493eda634cfe22251
Sumit Saini <sumitforsaken76248@gmail.com>
2026-01-31 23:02:06 +0530

    added_all_components_on_landing_page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,23 @@
 import FeatureSection from "@/components/myComponents/FeatureSection";
 import Figures from "@/components/myComponents/Figures";
 import HeroSection from "@/components/myComponents/HeroSection";
-import Image from "next/image";
+import HowItWorks from "@/components/myComponents/HowItWorks";
 
 export default function Home() {
   return (
     <div >
       <div className="grid-background"></div>
-      <HeroSection/>
-      <section className="w-full py-12 md:py-24 lg:py-32 bg-background">
-        <FeatureSection/>
+      <section className='w-full pt-36 md:pt-48 pb-10 '>
+        <HeroSection />
       </section>
       <section className="w-full py-12 md:py-24 lg:py-32 bg-background">
-        <Figures/>
+        <FeatureSection />
+      </section>
+      <section className="w-full py-12 md:py-24 lg:py-32 bg-muted">
+        <Figures />
+      </section>
+      <section className="w-full py-12 md:py-24 lg:py-32 bg-background">
+        <HowItWorks/>
       </section>
     </div>
   );
