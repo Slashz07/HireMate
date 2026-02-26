523a19dd7dc405d11799ea194d7a4adcef72f772
Sumit Saini <sumitforsaken76248@gmail.com>
2026-02-26 23:02:27 +0530

    created gemini api call as a separate library function

---
+++ src/lib/gemini.ts
@@ -0,0 +1,21 @@
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+
+
+export const getGeminiResponse=async(prompt:string)=>{
+     const response = await ai.models.generateContent({
+            model: "gemini-2.5-flash",
+            contents: prompt,
+        });
+
+        if (!response.text) throw new Error("Error accessing data in Gemini api");
+
+        const raw = response.text.trim();
+
+        // Extract JSON block
+        const match = raw.match(/\{[\s\S]*\}/);//match here extracts everything enclosed in {} in the raw and returns it in an array of object
+        if (!match) throw new Error("No JSON found in gemini api response");
+
+        return JSON.parse(match[0])
+}
