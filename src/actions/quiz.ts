ce680397718cc437d79d129f28ecdba4217cd2a6
Sumit Saini <sumitforsaken76248@gmail.com>
2026-02-28 12:18:13 +0530

    defined quiz as a server action

--- src/actions/quiz.ts
+++ src/actions/quiz.ts
@@ -1,3 +1,4 @@
+"use server"
 import { getGeminiResponse } from "@/lib/gemini";
 import { db } from "@/lib/prisma"
 import { auth } from "@clerk/nextjs/server"
@@ -19,7 +20,7 @@ export const generateQuiz = async () => {
     }
     try {
         const prompt = `
-         Generate 10 technical interview questions for a ${user.industry} professional ${user.skills?.length ? `with expertise in ${user.skills.join(",")}` : ""}
+         Generate 12 technical interview questions for a ${user.industry} professional ${user.skills?.length ? `with expertise in ${user.skills.join(",")}` : ""}
  
          Each question should be multiple choice with 4 options.
  
@@ -66,39 +67,44 @@ export const saveQuizData = async (questions, answers, score) => {
         explanation: q.explanation
     }))
 
-    const wrongQuestions = questionResults.filter((q) => !q.isCorrect)
-    let improvementTip=""
-    if (wrongQuestions.length > 0) {
-        const wrongQuestionsText = wrongQuestions.map((q) => (
-            `Question: "${q.question}"\nCorrect Answer: "${q.answer}"\nUser Answer:"${q.userAnswer}"`
-        )).join("\n\n")//.join() converts an array into a single string. .join("\n\n") here inserts double line breaks between each array elem when joining them
-
-        const improvementPrompt = `
-        The user got the following ${user.industry} techincal interview questions wrong:
-        ${wrongQuestionsText} 
-        Based on these mistakes, provide a concise, specific improvement tip.
-        Focus on the knowledge gaps revealed by these wrong answers.
-        Keep the response under 2-3 sentences and make it encourging.
-        Don't explicitly mention the mistakes, instead focus on what to learn/practice.
-    `
-        const response = await ai.models.generateContent({
-            model: "gemini-2.5-flash",
-            contents: improvementPrompt,
-        });
-
-        if (!response.text) throw new Error("Error accessing data in Gemini api");
-
-        improvementTip= response.text.trim();
-        
-    }
-    const assessment=await db.assessment.create({
-        data:{
-            userId:user.id,
-            quizScore:score,
-            questions:questionResults,
-            category:"Techincal",
-            improvementTip,
-        }
-    })
+   try {
+     const wrongQuestions = questionResults.filter((q) => !q.isCorrect)
+     let improvementTip=""
+     if (wrongQuestions.length > 0) {
+         const wrongQuestionsText = wrongQuestions.map((q) => (
+             `Question: "${q.question}"\nCorrect Answer: "${q.answer}"\nUser Answer:"${q.userAnswer}"`
+         )).join("\n\n")//.join() converts an array into a single string. .join("\n\n") here inserts double line breaks between each array elem when joining them
+ 
+         const improvementPrompt = `
+         The user got the following ${user.industry} techincal interview questions wrong:
+         ${wrongQuestionsText} 
+         Based on these mistakes, provide a concise, specific improvement tip.
+         Focus on the knowledge gaps revealed by these wrong answers.
+         Keep the response under 2-3 sentences and make it encourging.
+         Don't explicitly mention the mistakes, instead focus on what to learn/practice.
+     `
+         const response = await ai.models.generateContent({
+             model: "gemini-2.5-flash",
+             contents: improvementPrompt,
+         });
+ 
+         if (!response.text) throw new Error("Error accessing data in Gemini api");
+ 
+         improvementTip= response.text.trim();
+         
+     }
+     const assessment=await db.assessment.create({
+         data:{
+             userId:user.id,
+             quizScore:score,
+             questions:questionResults,
+             category:"Techincal",
+             improvementTip,
+         }
+     })
     return assessment
+
+   } catch (error) {
+    console.log("Error generating improvement tip and saving quiz data to database")
+   }
 }
