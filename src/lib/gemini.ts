963ff301b309f3eb6b9416c23aa4bc4725c31bd6
Sumit Saini <sumitforsaken76248@gmail.com>
2026-03-23 09:43:01 +0530

    updated library function accessing llm to recieve both text and json based response based on parameter provided

--- src/lib/gemini.ts
+++ src/lib/gemini.ts
@@ -3,19 +3,24 @@ import { GoogleGenAI } from "@google/genai";
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 
-export const getGeminiResponse=async(prompt:string)=>{
-     const response = await ai.models.generateContent({
-            model: "gemini-2.5-flash",
-            contents: prompt,
-        });
+export const getGeminiResponse = async (prompt: string, expectJson: boolean = true) => {
+    const response = await ai.models.generateContent({
+        model: "gemini-2.5-flash",
+        contents: prompt,
+    });
 
-        if (!response.text) throw new Error("Error accessing data in Gemini api");
+    if (!response.text) throw new Error("Error accessing data in Gemini api");
 
-        const raw = response.text.trim();
+    const raw = response.text.trim();
 
-        // Extract JSON block
+    // Extract JSON block if demanded during call
+    if (expectJson) {
         const match = raw.match(/\{[\s\S]*\}/);//match here extracts everything enclosed in {} in the raw and returns it in an array of object
         if (!match) throw new Error("No JSON found in gemini api response");
 
         return JSON.parse(match[0])
+    }else{
+        return raw
+    }
+
 }
